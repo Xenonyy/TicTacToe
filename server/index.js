64ef313608afe39155dcff71294fb6a5bc15851f
Xenon <gonda.armand@gmail.com>
2021-02-20 11:01:12 +0100

    Initial commit.

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const port = process.env.PORT || 3001;
+const app = express();
+const path = require('path');
+
+app.get("/api", (req, res) => {
+	res.json({ message: "Hello from server!" });
+});
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../react-ttt/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+	res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../react-ttt/build', 'index.html'));
+});
+
+app.listen(port, () => {
+	console.log(`Server listening on ${port}`);
+});
